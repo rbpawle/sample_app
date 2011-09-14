dbefb84c8814a324e4d4da0498a0189a82b1a6bb
Reginald Pawle <rbpawle@gmail.com>
2011-09-14 16:48:23 -0400

    modified title in application_helper.rb, modified custom.css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	# Return a title on a per-page basis.
 	def title
-		base_title = "Ruby on Rails Tutorial Sample App"
+		base_title = "Something different for a title"
 		if @title.nil?
 			base_title
 		else
